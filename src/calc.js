c7735e64edb4470ba26dc0a27483eb19a0296812
d-verm <15vermad@gmail.com>
2026-09-16 22:31:04 +0100

    add collision dataset validation

--- src/calc.js
+++ src/calc.js
@@ -1,6 +1,6 @@
 /**
  * src/calc.js
- * DfT Travel Carbon Calculator
+ * DfT Travel Carbon Calculatorrr
  *
  * This file contains the emissions calculation logic and a small embedded subset
  * of the UK Government GHG Conversion Factors (2025) needed for this MVP.
